fca9bda7f66072f0f1ee98931982b3b0a18e6ad0
Byron Gaskin <byron.gaskin@clypr.net>
2016-03-02 06:06:48 +0000

    added image popup gallery. also added placholder avatar on portfolio page

--- main.js
+++ main.js
@@ -5,4 +5,22 @@
   if (location.hash) shiftWindow();
   window.addEventListener("hashchange", shiftWindow);
 
+  // Magnific Popup
+  var magnifPopup = function() {
+    // This will create a single gallery from all elements that have class "gallery-item"
+    $('.gallery-item').magnificPopup({
+      type: 'image',
+      image: {
+        verticalFit: true
+      },
+      gallery:{
+        enabled:true
+      }
+    }); 
+  };
+
+  $(function(){
+    magnifPopup();
+  });
+
 })(jQuery); // End of use strict
